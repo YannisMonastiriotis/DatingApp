2b5540270d4b58ef084b9a262067f6ea8734e8df
Ioannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-09-15 19:45:43 +0300

    Added Certificate and Angular Skeletor

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,12 +33,8 @@ namespace API
             {
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
-            
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,13 +43,17 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseHttpsRedirection();
+              app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
             app.UseRouting();
+
 
             app.UseAuthorization();
 
